0d9c8c4d3f6bb8006a1f7e4be61df5160b044f42
Jefferson Carley <jeff.carley@gmail.com>
2015-11-14 10:33:51 -0600

    Adds on_success and on_error callbacks to the command result

--- lib/active_command/command_result.rb
+++ lib/active_command/command_result.rb
@@ -12,5 +12,18 @@ module ActiveCommand
     end
     alias_method :is_successful?, :success?
 
+    def on_success?(&block)
+      if self.is_successful?
+        block.call(self)
+      end
+      self
+    end
+
+    def on_error?(&block)
+      if ! self.is_successful?
+        block.call(self)
+      end
+      self
+    end
   end
 end
